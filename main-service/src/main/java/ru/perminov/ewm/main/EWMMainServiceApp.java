8bd67a343a85ec53937de093ff447ddc7becfc71
Андрей <Perminand@yandex.ru>
2024-09-01 11:22:16 +0300

    feat: Настроен компоус и папки

---
+++ main-service/src/main/java/ru/perminov/ewm/main/EWMMainServiceApp.java
@@ -0,0 +1,13 @@
+package ru.perminov.ewm.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWMMainServiceApp {
+
+    public static void main(String[] arg) {
+        SpringApplication.run(EWMMainServiceApp.class);
+
+    }
+}
