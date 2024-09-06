0025bf5f563977e6d96e2b3d2c94905a1a04e128
Андрей <Perminand@yandex.ru>
2024-09-06 16:57:12 +0300

    feat: Сделать JDBC репозиторий

--- stat/stat-dto/src/main/java/ru/practicum/ewm/model/dto/stat/ViewStatsDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/ewm/model/dto/stat/ViewStatsDto.java
@@ -1,14 +1,16 @@
 package ru.practicum.ewm.model.dto.stat;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @ToString
 public class ViewStatsDto {
     private String app;
-    private String url;
+    private String uri;
     private Long hits;
 }
--- stat/stat-server/src/main/java/ewm/server/repository/StatsRepository.java
+++ stat/stat-server/src/main/java/ewm/server/repository/StatsRepository.java
@@ -1,32 +1,18 @@
 package ewm.server.repository;
 
-import ewm.server.model.EndpointHit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import ru.practicum.ewm.EndpointHitDto;
+import ru.practicum.ewm.ViewsStatsRequest;
 import ru.practicum.ewm.model.dto.stat.ViewStatsDto;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
-    @Query("SELECT app, uri, COUNT (ip) AS hits FROM EndpointHit " +
-            "WHERE (timestamp >= :start AND timestamp <= :end) " +
-            "GROUP BY app, uri ORDER BY hits DESC")
-    List<ViewStatsDto> selectAllWhereCreatedAfterStartAndBeforeEnd(LocalDateTime start, LocalDateTime end);
+/**
+ * Интерфейс для работы с репозиторием статистики.
+ */
+public interface StatsRepository {
+    void saveHit(EndpointHitDto hit);
 
-    @Query("SELECT app, uri, COUNT (ip) AS hits FROM EndpointHit " +
-            "WHERE (timestamp >= :start AND timestamp <= :end AND uri IN :uris) " +
-            "GROUP BY app, uri ORDER BY hits DESC")
-    List<ViewStatsDto> selectAllWhereCreatedAfterStartAndBeforeEndUris(LocalDateTime start, LocalDateTime end, List<String> uris);
+    List<ViewStatsDto> getStats(ViewsStatsRequest request);
 
-    @Query("SELECT app, uri, COUNT (DISTINCT ip) AS hits FROM EndpointHit " +
-            "WHERE (timestamp >= :start AND timestamp <= :end AND uri IN :uris) " +
-            "GROUP BY app, uri ORDER BY hits DESC")
-    List<ViewStatsDto> selectAllWhereCreatedAfterStartAndBeforeEndUrisUnicum(LocalDateTime start, LocalDateTime end, List<String> uris);
-
-    @Query("SELECT app, uri, COUNT (DISTINCT ip) AS hits FROM EndpointHit " +
-            "WHERE (timestamp >= :start AND timestamp <= :end) " +
-            "GROUP BY app, uri ORDER BY hits DESC")
-    List<ViewStatsDto> selectAllWhereCreatedAfterStartAndBeforeEndUnicum(LocalDateTime start, LocalDateTime end);
+    List<ViewStatsDto> getUniqueStats(ViewsStatsRequest request);
 }
